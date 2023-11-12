a8347fcd1384fdc8ca4433a5e2c73144b2fa7830
Anton Ustyuzhanin <tony.brewerio@gmail.com>
2023-11-12 13:58:50 +0100

    wip: botched attempt at using super array for 1786
    Does not work, because there are test cases where there are thousands of edges per node.
    Works great for graphs where there are very little edges per node.

--- src/main/java/org/playground/leetcode/LeetCode1786.java
+++ src/main/java/org/playground/leetcode/LeetCode1786.java
@@ -4,7 +4,8 @@ import org.playground.leetcode.helpers.AveragingOperationTimer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.PriorityQueue;
 
 /**
  * @see <a href="https://leetcode.com/problems/design-graph-with-shortest-path-calculator/submissions/1096453667/">Submission</a>
@@ -14,78 +15,116 @@ public class LeetCode1786 {
     private AveragingOperationTimer timer;
     private static final long MODULO = (long) (Math.pow(10, 9) + 7);
 
+    private static class SuperGraph {
+        private static final int CHUNK = 1 + (5 * 2);
+        public final int n;
+        public final int[] edges;
+
+        public SuperGraph(int n, int[][] edges) {
+            // okay, so here's an idea, though it is probably stupid as hell
+            // I will attempt to put the entire graph into one large linear array, where each node is given a section
+            // for a full 20000 nodes times 5 edges graph it will be less than a single megabyte
+            // this will probably make things worse by screwing up cpu cache, but why not
+            this.n = n;
+            this.edges = new int[n * CHUNK];
+            for (int[] edge : edges) {
+                addEdgeToNode(edge[0] - 1, edge[1] - 1, edge[2]);
+                addEdgeToNode(edge[1] - 1, edge[0] - 1, edge[2]);
+            }
+        }
+
+        public void addEdgeToNode(int node, int next, int cost) {
+            int lower = node * CHUNK;
+            int count = edges[lower];
+            int offset = lower + 1 + count * 2;
+            edges[lower]++;
+            try {
+                edges[offset] = next;
+                edges[offset + 1] = cost;
+            } catch (ArrayIndexOutOfBoundsException exception) {
+                log.info("node {} failed to add an edge, has {} edges", node, count + 1);
+            }
+        }
+
+        public int edgeCountByNode(int node) {
+            int lower = node * CHUNK;
+            return edges[lower];
+        }
+
+        public int edgeNextByNode(int node, int edge) {
+            int lower = node * CHUNK;
+            return edges[lower + 1 + edge * 2];
+        }
+
+        public int edgeCostByNode(int node, int edge) {
+            int lower = node * CHUNK;
+            return edges[lower + 1 + edge * 2 + 1];
+        }
+    }
+
+
     public int countRestrictedPaths(int n, int[][] edgesArray) {
-        timer = new AveragingOperationTimer(log, "setup - {} micros");
-        List<List<Edge>> graph = new ArrayList<>(n);
-        for (int i = 0; i < n; i++) {
-            graph.add(new ArrayList<>(4));
-        }
-        for (int[] edge : edgesArray) {
-            graph.get(edge[0] - 1).add(new Edge(edge[1] - 1, edge[2]));
-            graph.get(edge[1] - 1).add(new Edge(edge[0] - 1, edge[2]));
-        }
+        timer = new AveragingOperationTimer(log, "sg setup - {} micros");
+        var sg = new SuperGraph(n, edgesArray);
         timer.close();
-        //
+        timer = new AveragingOperationTimer(log, "setup - {} micros");
         timer = new AveragingOperationTimer(log, "distances - {} micros");
-        var distances = calculateDistances(graph);
+        var distances = calculateDistances(sg);
         timer.close();
         timer = new AveragingOperationTimer(log, "counts - {} micros");
-        var count = countRestrictedPathsDpRec(graph, distances);
+        var count = countRestrictedPathsDpRec(sg, distances);
         timer.close();
         return count;
     }
 
-    public int[] calculateDistances(List<List<Edge>> graph) {
+    private int[] calculateDistances(SuperGraph sg) {
         var paths = new PriorityQueue<CalcDistancePath>();
-        paths.add(new CalcDistancePath(graph.size() - 1, 0));
-        var distances = new int[graph.size()];
+        paths.add(new CalcDistancePath(sg.n - 1, 0));
+        var distances = new int[sg.n];
         Arrays.fill(distances, Integer.MAX_VALUE);
-        distances[graph.size() - 1] = 0;
+        distances[sg.n - 1] = 0;
         while (!paths.isEmpty()) {
             var path = paths.poll();
             var pathNode = path.node();
             var pathCost = path.cost();
-            if (distances[pathNode] != pathCost) {
+            if (pathCost > distances[pathNode]) {
                 continue;
             }
-            for (var edge : graph.get(pathNode)) {
-                int edgeNext = edge.next();
-                int edgeCost = edge.cost();
-                var edgeDistance = distances[pathNode] + edgeCost;
+            for (var edge = 0; edge < sg.edgeCountByNode(pathNode); edge++) {
+                int edgeNext = sg.edgeNextByNode(pathNode, edge);
+                int edgeCost = sg.edgeCostByNode(pathNode, edge);
+                var edgeDistance = pathCost + edgeCost;
                 if (distances[edgeNext] > edgeDistance) {
                     distances[edgeNext] = edgeDistance;
-                    paths.add(new CalcDistancePath(edgeNext, edgeCost + pathCost));
+                    paths.add(new CalcDistancePath(edgeNext, edgeDistance));
                 }
             }
         }
         return distances;
     }
 
-    private int countRestrictedPathsDpRec(List<List<Edge>> graph, int[] distances) {
-        var counts = new long[graph.size()];
+    private int countRestrictedPathsDpRec(SuperGraph sg, int[] distances) {
+        var counts = new long[sg.n];
         Arrays.fill(counts, -1);
-        counts[graph.size() - 1] = 1;
-        return (int) countRestrictedPathsDpRec(graph, distances, counts, 0);
+        counts[sg.n - 1] = 1;
+        return (int) countRestrictedPathsDpRec(sg, distances, counts, 0);
     }
 
-    private long countRestrictedPathsDpRec(List<List<Edge>> graph, int[] distances, long[] counts, int node) {
+    private long countRestrictedPathsDpRec(SuperGraph sg, int[] distances, long[] counts, int node) {
         long count = counts[node];
         if (count != -1) {
             return count;
         }
         count = 0;
-        for (Edge edge : graph.get(node)) {
-            int edgeNext = edge.next();
+        for (var edge = 0; edge < sg.edgeCountByNode(node); edge++) {
+            var edgeNext = sg.edgeNextByNode(node, edge);
             if (distances[node] > distances[edgeNext]) {
-                count += countRestrictedPathsDpRec(graph, distances, counts, edgeNext);
+                count += countRestrictedPathsDpRec(sg, distances, counts, edgeNext);
                 count = count % MODULO;
             }
         }
         counts[node] = count;
         return count;
-    }
-
-    public record Edge(int next, int cost) {
     }
 
     public record CalcDistancePath(int node, int cost) implements Comparable<CalcDistancePath> {
