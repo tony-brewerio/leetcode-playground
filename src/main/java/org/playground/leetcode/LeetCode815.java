3779fcb52600c6d70dec1bb842589bfeee2c9dc7
Anton Ustyuzhanin <tony.brewerio@gmail.com>
2023-11-13 01:31:29 +0100

    accepted solution for 815, works pretty well

--- src/main/java/org/playground/leetcode/LeetCode815.java
+++ src/main/java/org/playground/leetcode/LeetCode815.java
@@ -1,17 +1,18 @@
 package org.playground.leetcode;
 
-import org.playground.leetcode.helpers.AveragingOperationTimer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.util.*;
+import java.util.Arrays;
+import java.util.BitSet;
+import java.util.PriorityQueue;
 
 /**
- * @see <a href="https://leetcode.com/problems/restore-the-array-from-adjacent-pairs/submissions/1096232787/">Submission</a>
+ * @see <a href="https://leetcode.com/problems/bus-routes/submissions/1097536372/">Submission</a>
+ * Runtime: 95 ms, Beats 57.06% of users with Java
+ * Memory: 58.39 MB, Beats 94.26% of users with Java
  * <p>
  * Problem notes.
  * There are at most 500 buses overall. We can easily use BitSet or shorts perhaps?
  * As usual, Leetcode provies 1 base numbers, which sometimes kind of sucks.
+ * Bus stops per route are always sorted, or at least we can sort them to save up some time later?
  * <p>
  * Constraints:
  * 1 <= routes.length <= 500.
@@ -21,122 +22,123 @@ import java.util.*;
  * 0 <= routes[i][j] < 10^6
  * 0 <= source, target < 10^6
  * <p>
+ * Attemped this task from the wrong angle at first, trying to treat both riding the bus and switching buses as edges.
+ * This does not work, since some bus routes are very very long, so you always fail execution time constraint.
+ * After a couple of pretty dumb attempts to make bazillion edges work, I've realized that there is no point.
+ * Since we only really need to return a number of bus hops, we can instead build a graph where edges are bus transfers.
+ * And, we convert source and target stop to arrays of buses, picking all buses that stop at source or target.
+ * Thus the problem is reduced to searching shortest path in a graph of bus transfers.
+ * There are not that many, comparatively speaking, transfers, so actual distance ranking is plenty fast.
+ * <p>
+ * Instead, you get the problem of how to compile the graph efficiently.
+ * I have decided to just iterate over all possible stop numbers, and at each step find all buses that stop there.
+ * Then, those buses are combined, each bus getting connections to all other buses.
+ * BitSet makes this very easy and decently fast.
+ * There are some unsorted routes in LeetCode test cases, so I have to presort them.
+ * <p>
  * Notes.
- * Doing -1 all the time is very awkward. Unless I use flat array arithmetic for something, it is kind of pointless.
  * Bus stops are treated as starting from zero, as they are based off of routes index.
  * Stops are 1 based, since I just treated them as is.
+ * BitSets are very memory efficient, which is not surprising.
  */
 public class LeetCode815 {
-    private static final Logger log = LoggerFactory.getLogger(LeetCode815.class);
-    private AveragingOperationTimer timer;
 
     public int numBusesToDestination(int[][] routes, int source, int target) {
         if (source == target) {
             return 0;
         }
-        // By definition, there are less than 10^6 individual bus stops and at most 500 routes.
-        // Thus, we can represent each unique stop at each route by a single number under 50_000_000.
-        // Basically, stop id will be bus_n * 500 + stop_n.
-        // We can make an array of ints of this size, but it will cost like 400 MB, which is probably too much.
-        // Still, regular int should be a tad bit faster overall than POJO.
-        log.info("=== LeetCode 815 ===");
-        timer = new AveragingOperationTimer(log, "setup - {} ms");
-        int maxStopNumber = -1;
         for (int[] route : routes) {
-            for (int stop : route) {
-                if (stop > maxStopNumber) {
-                    maxStopNumber = stop;
+            Arrays.sort(route);
+        }
+        var sources = new BitSet();
+        var targets = new BitSet();
+        for (int i = 0; i < routes.length; i++) {
+            var route = routes[i];
+            if (Arrays.binarySearch(route, source) >= 0) {
+                sources.set(i);
+            }
+            if (Arrays.binarySearch(route, target) >= 0) {
+                targets.set(i);
+            }
+        }
+        // this relies on the fact that input were all sorted
+        // the idea is that we iterate over all the possible stop numbers
+        // then scan routes from the start, looking for the number
+        // all buses where stop was found are collected together
+        var edges = new BitSet[routes.length];
+        {
+            for (int i = 0; i < routes.length; i++) {
+                edges[i] = new BitSet(routes.length);
+            }
+            int[] offsets = new int[routes.length];
+            for (int stop = 0; stop < 1_000_000; stop++) {
+                var buses = new BitSet(routes.length);
+                boolean end = true;
+                for (int bus = 0; bus < offsets.length; bus++) {
+                    int[] route = routes[bus];
+                    while (offsets[bus] < route.length && route[offsets[bus]] < stop) {
+                        offsets[bus]++;
+                    }
+                    if (offsets[bus] == route.length) {
+                        continue;
+                    } else {
+                        end = false;
+                    }
+                    if (route[offsets[bus]] == stop) {
+                        buses.set(bus);
+                    }
+                }
+                if (end) {
+                    break;
+                }
+                for (int i = buses.nextSetBit(0); i != -1; i = buses.nextSetBit(i + 1)) {
+                    edges[i].or(buses);
                 }
             }
         }
-        BitSet[] transfers = new BitSet[maxStopNumber + 1];
-        for (int bus = 0; bus < routes.length; bus++) {
-            int[] route = routes[bus];
-            for (int stop : route) {
-                var tr = transfers[stop];
-                if (tr == null) {
-                    tr = transfers[stop] = new BitSet(routes.length);
-                }
-                tr.set(bus);
-            }
-        }
-        timer.close();
-        timer = new AveragingOperationTimer(log, "array - {} ms");
-        long[][] distances = new long[routes.length][maxStopNumber + 1];
-        timer.close();
-        timer = new AveragingOperationTimer(log, "distances - {} ms");
-        var result = minHopsToDestination2(routes, transfers, distances, ((long) maxStopNumber) * routes.length, source, target);
-        timer.close();
-        return result;
+        return minHopsToDestination(edges, sources, targets);
     }
 
-    private int minHopsToDestination2(int[][] routes,
-                                      BitSet[] transfers,
-                                      long[][] distances,
-                                      long busHopEdgeCost,
-                                      int source,
-                                      int target) {
+    private int minHopsToDestination(BitSet[] edges, BitSet sources, BitSet targets) {
         // we start our stack with one hop on every bus on the source stop
+        int[] distances = new int[edges.length];
+        Arrays.fill(distances, Integer.MAX_VALUE);
         var stack = new PriorityQueue<CalcDistancePath>();
-        transfers[source].stream().forEach(bus -> {
-            var stop = new Stop(bus, source);
-            stack.add(new CalcDistancePath(stop, busHopEdgeCost));
-            distances[stop.bus()][stop.stop()] = busHopEdgeCost;
+        sources.stream().forEach(bus -> {
+            stack.add(new CalcDistancePath(bus, 1));
+            distances[bus] = 1;
         });
-        long bestEndStopDistance = Long.MAX_VALUE;
         while (!stack.isEmpty()) {
             var prev = stack.poll();
-            var prevStopStop = prev.stop().stop();
-            var prevStopBus = prev.stop().bus();
+            var prevBus = prev.bus();
             var prevCost = prev.cost();
-            var currPrevStopDistance = distances[prevStopBus][prevStopStop];
-            if (currPrevStopDistance != 0 && (prevCost > bestEndStopDistance || prevCost > currPrevStopDistance)) {
+            if (prevCost > distances[prevBus]) {
                 continue;
             }
-            // start with following the same bus route that we are on, with cost = 1
-            for (int nextStopStop : routes[prevStopBus]) {
-                if (nextStopStop != prevStopStop) {
-                    var nextStop = new Stop(prevStopBus, nextStopStop);
-                    var nextStopBus = nextStop.bus();
-                    var nextStopCost = prevCost + 1;
-                    var currNextStopDistance = distances[nextStopBus][nextStopStop];
-                    if (currNextStopDistance == 0 || currNextStopDistance > nextStopCost) {
-                        distances[nextStopBus][nextStopStop] = nextStopCost;
-                        stack.add(new CalcDistancePath(nextStop, nextStopCost));
-                        if (nextStopStop == target && nextStopCost < bestEndStopDistance) {
-                            bestEndStopDistance = nextStopCost;
-                        }
-                    }
+            var nextBusSet = edges[prevBus];
+            for (int nextBus = nextBusSet.nextSetBit(0); nextBus != -1; nextBus = nextBusSet.nextSetBit(nextBus + 1)) {
+                if (nextBus == prevBus) {
+                    continue;
                 }
-            }
-            // then follow all bus hops from the current stop, with cost = busHopEdgeCost
-            var tr = transfers[prevStopStop];
-            for (int nextStopBus = tr.nextSetBit(0); nextStopBus != -1; nextStopBus = tr.nextSetBit(nextStopBus + 1)) {
-                if (nextStopBus != prevStopBus) {
-                    var nextStop = new Stop(nextStopBus, prevStopStop);
-                    var nextStopStop = nextStop.stop();
-                    var nextStopCost = prevCost + busHopEdgeCost;
-                    var nextStopDistance = distances[nextStopBus][nextStopStop];
-                    if (nextStopDistance == 0 || nextStopDistance > nextStopCost) {
-                        distances[nextStopBus][nextStopStop] = nextStopCost;
-                        stack.add(new CalcDistancePath(nextStop, nextStopCost));
-                        if (prevStopBus == target && nextStopCost < bestEndStopDistance) {
-                            bestEndStopDistance = nextStopCost;
-                        }
-                    }
+                var nextCost = prevCost + 1;
+                if (nextCost < distances[nextBus]) {
+                    distances[nextBus] = nextCost;
+                    stack.add(new CalcDistancePath(nextBus, nextCost));
                 }
             }
         }
-        return (int) (bestEndStopDistance != Long.MAX_VALUE ? bestEndStopDistance / busHopEdgeCost : -1);
+        return targets.stream()
+                .map(bus -> distances[bus])
+                .filter(d -> d != Integer.MAX_VALUE)
+                .min()
+                .orElse(-1);
     }
 
-    public record CalcDistancePath(Stop stop, long cost) implements Comparable<CalcDistancePath> {
+    public record CalcDistancePath(int bus, int cost) implements Comparable<CalcDistancePath> {
         @Override
         public int compareTo(CalcDistancePath o) {
-            return Long.compare(cost, o.cost);
+            return cost - o.cost;
         }
     }
 
-    public record Stop(int bus, int stop) {
-    }
 }
