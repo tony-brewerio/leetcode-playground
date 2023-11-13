c5c0d8eaa2578b2fe71e5b0dcabf37d91a97323a
Anton Ustyuzhanin <tony.brewerio@gmail.com>
2023-11-13 03:49:10 +0100

    accepted solution for 2785, use counting sort, super fast

--- src/main/java/org/playground/leetcode/LeetCode2785.java
+++ src/main/java/org/playground/leetcode/LeetCode2785.java
@@ -1,11 +1,9 @@
 package org.playground.leetcode;
 
-import java.util.Arrays;
-
 /**
  * @see <a href="https://leetcode.com/problems/sort-vowels-in-a-string/submissions/1097578500/">Submission</a>
- * Runtime: 11 ms, Beats 99.28% of users with Java
- * Memory: 45.41 MB, Beats 63.70% of users with Java
+ * Runtime: 5 ms, Beats 100.00% of users with Java
+ * Memory: 43.52 MB, Beats 100.00% of users with Java
  * <p>
  * Constraints:
  * 1 <= s.length <= 10^5 (100_000)
@@ -15,47 +13,46 @@ import java.util.Arrays;
  * Initially, I've used ArrayList to store vowels, but sorting ArrayList takes like 50 time longer than char[].
  * After checking other solutions, and trying it out, it seems obvious that doing second run over the string is not great.
  * Recording vowel positions and slotting vowels back from them is much faster than another iteration over the whole string.
- * The end solution is very fast, but relatively not memory efficient, since arrays are pre-allocated greedily.
+ * <p>
+ * After looking over the recommended approach, updated the code to use counting sort instead.
+ * An interesting note is that using boolean array to test chars for vowels is much faster than if tree.
  */
 public class LeetCode2785 {
 
     public String sortVowels(String str) {
         char[] chars = str.toCharArray();
-        char[] vowels = new char[chars.length];
+        // The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in lowercase or uppercase.
+        boolean[] vowels = new boolean[127];
+        vowels['A'] = true;
+        vowels['E'] = true;
+        vowels['I'] = true;
+        vowels['O'] = true;
+        vowels['U'] = true;
+        vowels['a'] = true;
+        vowels['e'] = true;
+        vowels['i'] = true;
+        vowels['o'] = true;
+        vowels['u'] = true;
+        int[] counts = new int[127];
         int[] positions = new int[chars.length];
         int vl = 0;
         for (int i = 0; i < chars.length; i++) {
             var c = chars[i];
-            if (isAsciiVowel(c)) {
-                vowels[vl] = c;
-                positions[vl] = i;
-                vl++;
+            if (vowels[c]) {
+                counts[c]++;
+                positions[vl++] = i;
             }
         }
         if (vl == 0) {
             return str;
         }
-        vowels = Arrays.copyOf(vowels, vl);
-        positions = Arrays.copyOf(positions, vl);
-        Arrays.sort(vowels);
-        for (int i = 0; i < vl; i++) {
-            chars[positions[i]] = vowels[i];
+        int vi = 0;
+        for (var vowel : new char[]{'A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u'}) {
+            for (int i = 0; i < counts[vowel]; i++) {
+                chars[positions[vi++]] = vowel;
+            }
         }
-        return String.valueOf(chars);
-    }
-
-    private static boolean isAsciiVowel(char c) {
-        // The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in lowercase or uppercase.
-        return c == 'A'
-                || c == 'E'
-                || c == 'I'
-                || c == 'O'
-                || c == 'U'
-                || c == 'a'
-                || c == 'e'
-                || c == 'i'
-                || c == 'o'
-                || c == 'u';
+        return new String(chars);
     }
 
 }
