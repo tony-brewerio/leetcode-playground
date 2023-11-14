e49ee4dcf8526f0f1d6cd49bce89a2621eae2aa9
Anton Ustyuzhanin <tony.brewerio@gmail.com>
2023-11-14 21:26:19 +0100

    accepted solution for 1930, optimized, very fast

--- src/main/java/org/playground/leetcode/LeetCode1930.java
+++ src/main/java/org/playground/leetcode/LeetCode1930.java
@@ -1,32 +1,33 @@
 package org.playground.leetcode;
 
-import java.util.*;
+import java.util.BitSet;
 
 /**
+ *
  */
 public class LeetCode1930 {
+    public static final String ASCII_LOWERCASE_LETTERS_STR = "abcdefghijklmnopqrstuvwxyz";
 
     public int countPalindromicSubsequence(String s) {
         // a-z ASCII codes are 97-122
-        List<BitSet> positions = new ArrayList<>(123);
-        for (int i = 0; i < 123; i++) {
-            positions.add(i >= 97 ? new BitSet(s.length()) : null);
+        BitSet[] positions = new BitSet[123];
+        for (int i = 97; i < 123; i++) {
+            positions[i] = new BitSet(s.length());
         }
         // mark position indexes by char
         for (int i = 0; i < s.length(); i++) {
-            positions.get(s.charAt(i)).set(i);
+            positions[s.charAt(i)].set(i);
         }
         //
-        var palindromes = new HashSet<Palindrome>((int) Math.pow(123 - 97, 3));
-        //
+        var palindromes = new BitSet();
         for (char c1 = 97; c1 < 123; c1++) {
-            var c1p = positions.get(c1);
+            var c1p = positions[c1];
             if (c1p.isEmpty()) {
                 continue;
             }
             var c1i = c1p.nextSetBit(0);
             for (char c2 = 97; c2 < 123; c2++) {
-                var c2p = positions.get(c2);
+                var c2p = positions[c2];
                 if (c2p.isEmpty()) {
                     continue;
                 }
@@ -34,23 +35,17 @@ public class LeetCode1930 {
                 if (c2i == -1) {
                     continue;
                 }
-                for (char c3 = c1; c3 <= c1; c3++) {
-                    var c3p = positions.get(c3);
-                    if (c3p.isEmpty()) {
-                        continue;
-                    }
-                    int c3i = c3p.nextSetBit(c2i + 1);
-                    if (c3i == -1) {
-                        continue;
-                    }
-                    palindromes.add(new Palindrome(c1, c2, c3));
+                int c3i = c1p.nextSetBit(c2i + 1);
+                if (c3i == -1) {
+                    continue;
                 }
+                palindromes.set((c1 << 8) + c2);
             }
         }
-        return palindromes.size();
+        int result = 0;
+        for (var i = palindromes.nextSetBit(0); i != -1; i = palindromes.nextSetBit(i + 1)) {
+            result++;
+        }
+        return result;
     }
-
-    private record Palindrome(char c1, char c2, char c3) {
-    }
-
 }
