f52ff186ed933c772cb9c80d0cbef200b01ca177
Anton Ustyuzhanin <tony.brewerio@gmail.com>
2023-11-14 21:56:49 +0100

    accepted solution for 1930, optimize a little more

--- src/main/java/org/playground/leetcode/LeetCode1930.java
+++ src/main/java/org/playground/leetcode/LeetCode1930.java
@@ -3,7 +3,14 @@ package org.playground.leetcode;
 import java.util.BitSet;
 
 /**
- *
+ * @see <a href="https://leetcode.com/problems/unique-length-3-palindromic-subsequences/submissions/1098923650/">Submission</a>
+ * Runtime: 15 ms, Beats 99.01% of users with Java
+ * Memory: 44.34 MB, Beats 52.85% of users with Java
+ * <p>
+ * The approach is very similar to a counting sort.
+ * First, do a pass over the string to record which positions for all characters.
+ * After that, we can simple loop over all possible combinations and check if they can be found based on positions.
+ * Runs really fast, and fairly efficient.
  */
 public class LeetCode1930 {
     public static final String ASCII_LOWERCASE_LETTERS_STR = "abcdefghijklmnopqrstuvwxyz";
@@ -19,7 +26,7 @@ public class LeetCode1930 {
             positions[s.charAt(i)].set(i);
         }
         //
-        var palindromes = new BitSet();
+        var palindromes = new BitSet(127 * 127);
         for (char c1 = 97; c1 < 123; c1++) {
             var c1p = positions[c1];
             if (c1p.isEmpty()) {
@@ -39,7 +46,7 @@ public class LeetCode1930 {
                 if (c3i == -1) {
                     continue;
                 }
-                palindromes.set((c1 << 8) + c2);
+                palindromes.set((c1 << 7) + c2);
             }
         }
         int result = 0;
