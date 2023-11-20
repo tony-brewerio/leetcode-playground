213bf421d8a98edd7ace08757b4dac66afef63d2
Anton Ustyuzhanin <tony.brewerio@gmail.com>
2023-11-20 23:38:54 +0100

    accepted solution for 2391, optimized some, still not great

--- src/main/java/org/playground/leetcode/LeetCode2931.java
+++ src/main/java/org/playground/leetcode/LeetCode2931.java
@@ -8,12 +8,12 @@ import java.util.BitSet;
 
 /**
  * @see <a href="https://leetcode.com/problems/minimum-amount-of-time-to-collect-garbage/submissions/1103021430/">Submission</a>
- * Runtime: 18 ms, Beats 72.57% of users with Java
- * Memory: 70.18 MB, Beats 8.76% of users with Java
+ * Runtime: 17 ms, Beats 74.67% of users with Java
+ * Memory: 62.40 MB, Beats 36.57% of users with Java
  * <p>
  * The description was a bit unclear, but the actual task is not too bad.
- * Is it worth prebuilding anything though? Feels like it is not really, but the memory was very bad relatively.
- * So perhaps most don't do this?
+ * Got a working solution that has only one actual if condition, explicitly allocates very little.
+ * Not sure why memory results are so bad.
  */
 public class LeetCode2931 {
     private final Logger log = LoggerFactory.getLogger(LeetCode2931.class);
@@ -24,31 +24,48 @@ public class LeetCode2931 {
         trucks['M'] = 0;
         trucks['P'] = 1;
         trucks['G'] = 2;
-        // amount of garbage per type per stop
-        var counts = new int[3][garbage.length];
-        //
-        for (int i = 0; i < garbage.length; i++) {
-            var g = garbage[i];
+        // reuse the same counts array so that it is allocated once
+        var counts = new int[3];
+        // all trucks start at stop 0
+        // state[0] = truck position
+        // state[1] = truck cost
+        // the first step is "unrolled" from the loop to remove an if condition from the loop, not sure if helps, but why not
+        var states = new int[3][2];
+        {
+            // here counts is zero-initialized
+            var g = garbage[0];
             for (int j = 0; j < g.length(); j++) {
-                counts[trucks[g.charAt(j)]][i]++;
+                counts[trucks[g.charAt(j)]]++;
+            }
+            for (int t = 0; t <= 2; t++) {
+                states[t][1] = counts[t];
             }
         }
-        // t = truck
-        int total = 0;
-        for (int t = 0; t <= 2; t++) {
-            int time = counts[t][0];
-            int pos = 0;
-            for (int i = 1; i < garbage.length; i++) {
-                var count = counts[t][i];
+        //
+        for (int i = 1; i < garbage.length; i++) {
+            // here we need to reset counts
+            counts[0] = counts[1] = counts[2] = 0;
+            var g = garbage[i];
+            for (int j = 0; j < g.length(); j++) {
+                counts[trucks[g.charAt(j)]]++;
+            }
+            //
+            for (int t = 0; t <= 2; t++) {
+                var count = counts[t];
+                var state = states[t];
                 if (count > 0) {
-                    time += count;
-                    for (int j = pos; j < i; j++) {
-                        time += travel[j];
+                    state[1] += count;
+                    for (int j = state[0]; j < i; j++) {
+                        state[1] += travel[j];
                     }
-                    pos = i;
+                    state[0] = i;
                 }
             }
-            total += time;
+        }
+        //
+        int total = 0;
+        for (int t = 0; t <= 2; t++) {
+            total += states[t][1];
         }
         return total;
     }
