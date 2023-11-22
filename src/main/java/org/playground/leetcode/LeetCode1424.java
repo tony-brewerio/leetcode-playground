71ea909d6c580a6a7cc7ebd9f426f4c4b89c86f0
Anton Ustyuzhanin <tony.brewerio@gmail.com>
2023-11-23 00:17:27 +0100

    accepted solution for 1424, better, but not quite that good

--- src/main/java/org/playground/leetcode/LeetCode1424.java
+++ src/main/java/org/playground/leetcode/LeetCode1424.java
@@ -4,50 +4,53 @@ import org.playground.leetcode.helpers.AveragingOperationTimer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 /**
- * @see <a href="https://leetcode.com/problems/diagonal-traverse-ii/submissions/1104483391/">Submission</a>
- * not accepted
+ * @see <a href="https://leetcode.com/problems/diagonal-traverse-ii/submissions/1104512066/">Submission</a>
+ * Runtime: 27 ms, Beats 75.00% of users with Java
+ * Memory: 75.16 MB, Beats 35.95% of users with Java
  * <p>
- * Would be cute if worked, but alas.
- * Leetcode apparently lied about the constraints.
- * They say that `nums[i][j] <= 10^5`, but that's not actually true, and test case 53 contains much larger values.
+ * Much better runtime, worse memory.
+ * Still kinda not that fast though?
  */
 public class LeetCode1424 {
 
     public int[] findDiagonalOrder(List<List<Integer>> nums) {
-        // max long is 9,223,372,036,854,775,807
-        //               200,000,100,000,100,000
-        // max item 100,000
-        // max rows 100,000
-        // max cols 100,000
-        // max sum  200,000
-        // six digits for sum, six digits for row, six digits for value
-        // 200,000,100,000,100,000
+        Map<Integer, int[]> diagonals = new HashMap<>();
         int total = 0;
-        for (int y = 0; y < nums.size(); y++) {
-            total += nums.get(y).size();
-        }
-        var longs = new long[total];
-        long maxval = 100_000;
-        long s_mult = 1_000_000_000_000L;
-        long y_mult = 1_000_000L;
-        int li = 0;
-        for (int y = 0; y < nums.size(); y++) {
+        for (int y = nums.size() - 1; y >= 0; y--) {
             var row = nums.get(y);
             for (int x = 0; x < row.size(); x++) {
-                longs[li++] = ((x + y) * s_mult) + ((maxval - y) * y_mult) + row.get(x);
+                int sum = x + y;
+                int val = row.get(x);
+                var diag = diagonals.get(sum);
+                if (diag != null) {
+                    if (diag[0] == diag.length) {
+                        diag = Arrays.copyOf(diag, (int) (diag.length * 1.4 + 8));
+                        diagonals.put(sum, diag);
+                    }
+                    diag[diag[0]++] = val;
+                    total++;
+                } else {
+                    diag = new int[8];
+                    diag[0] = 1;
+                    diag[diag[0]++] = val;
+                    total++;
+                    diagonals.put(sum, diag);
+                }
             }
         }
-        Arrays.sort(longs);
-        var ints = new int[longs.length];
-        for (int i = 0; i < longs.length; i++) {
-            ints[i] = (int) (longs[i] % y_mult);
+        var sums = diagonals.keySet().toArray(Integer[]::new);
+        Arrays.sort(sums);
+        var accum = new int[total];
+        var ai = 0;
+        for (Integer sum : sums) {
+            var diag = diagonals.get(sum);
+            for (int i = 1; i < diag[0]; i++) {
+                accum[ai++] = diag[i];
+            }
         }
-        return ints;
+        return accum;
     }
 }
