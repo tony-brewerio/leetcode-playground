63d439b86cce584df6076e36b16a60fbe3abb9fe
Anton Ustyuzhanin <tony.brewerio@gmail.com>
2023-11-22 22:47:33 +0100

    accepted solution for 1424, much faster now, but not that fast

--- src/main/java/org/playground/leetcode/LeetCode1424.java
+++ src/main/java/org/playground/leetcode/LeetCode1424.java
@@ -1,59 +1,29 @@
 package org.playground.leetcode;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 /**
- * @see <a href="https://leetcode.com/problems/diagonal-traverse-ii/submissions/1104475202/">Submission</a>
- * Runtime: 2593 ms, Beats 5.06% of users with Java
- * Memory: 66.26 MB, Beats 76.65% of users with Java
+ * @see <a href="https://leetcode.com/problems/diagonal-traverse-ii/submissions/1104483391/">Submission</a>
+ * Runtime: 45 ms, Beats 20.56% of users with Java
+ * Memory: 68.63 MB, Beats 66.74% of users with Java
  * <p>
- * Not entirely sure how to optimize this, even with hints.
- * The solution is very straightforward and brute force-y.
- * Barely squeaks by with terrible runtime score, had to convert input to arrays to avoid timeouts.
+ * Used the hints, but not checked any other solutions for now.
+ * Now its plenty fast, but still much slower than what other people have.
  */
 public class LeetCode1424 {
 
     public int[] findDiagonalOrder(List<List<Integer>> nums) {
-        int[][] arr = new int[nums.size()][];
-        for (int i = 0; i < nums.size(); i++) {
-            arr[i] = listOfIntegersToIntArray(nums.get(i));
-        }
-        int maxlen = 0;
-        int total = 0;
-        for (int[] row : arr) {
-            int rs = row.length;
-            if (rs > maxlen) {
-                maxlen = rs;
-            }
-            total += rs;
-        }
-        int[] accum = new int[total];
-        int ai = 0;
-        for (int r = 0; r < arr.length; r++) {
-            for (int x = 0, y = r; x < maxlen && y >= 0; x++, y--) {
-                var row = arr[y];
-                if (x < row.length) {
-                    accum[ai++] = row[x];
-                }
+        var tuples = new ArrayList<int[]>();
+        for (int y = 0; y < nums.size(); y++) {
+            var row = nums.get(y);
+            for (int x = 0; x < row.size(); x++) {
+                tuples.add(new int[]{x + y, -y, row.get(x)});
             }
         }
-        for (int c = 1; c <= maxlen; c++) {
-            for (int x = c, y = arr.length - 1; x < maxlen && y >= 0; x++, y--) {
-                var row = arr[y];
-                if (x < row.length) {
-                    accum[ai++] = row[x];
-                }
-            }
+        tuples.sort(Comparator.comparingInt((int[] t) -> t[0]).thenComparingInt((int[] t) -> t[1]));
         }
-        return accum;
-    }
-
-    private static int[] listOfIntegersToIntArray(List<Integer> list) {
-        int[] array = new int[list.size()];
-        int i = 0;
-        for (int num : list) {
-            array[i++] = num;
-        }
-        return array;
+        return tuples.stream().mapToInt(t -> t[2]).toArray();
     }
 }
