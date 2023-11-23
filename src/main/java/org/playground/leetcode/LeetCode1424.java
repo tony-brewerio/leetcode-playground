183e85ed83ea2e5660d94a4e21eb512740a1ea28
Anton Ustyuzhanin <tony.brewerio@gmail.com>
2023-11-23 01:17:13 +0100

    accepted solution for 1424, as fast as it gets

--- src/main/java/org/playground/leetcode/LeetCode1424.java
+++ src/main/java/org/playground/leetcode/LeetCode1424.java
@@ -4,39 +4,59 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
- * @see <a href="https://leetcode.com/problems/diagonal-traverse-ii/submissions/1104519889/">Submission</a>
- * Runtime: 21 ms, Beats 87.29% of users with Java
- * Memory: 61.82 MB, Beats 93.80% of users with Java
+ * @see <a href="https://leetcode.com/problems/diagonal-traverse-ii/submissions/1104531523/">Submission</a>
+ * Runtime: 11 ms, Beats 100.00% of users with Java
+ * Memory: 63.24 MB, Beats 88.12% of users with Java
  * <p>
- * Very fast and memory efficient.
- * Storing tuples in an array and sorting them with a lambda and not Comparator is a bit faster.
+ * Seems obvious in retrospect.
+ * This is identical to a previous HashMap solution, which was sorta fast but not that fast, and was not very memory efficient.
+ * However, we can also use a flat array to store all diagonals, since max diagonal sum of x+y is not that large.
+ * The resulting solution operates on int arrays as much as possible.
+ * Also, switched the row iteration order to reverse one. This way diagonals get naturally ordered as we want.
  */
 public class LeetCode1424 {
 
     public int[] findDiagonalOrder(List<List<Integer>> nums) {
         int total = 0;
+        int maxrs = 0;
         for (var row : nums) {
-            total += row.size();
-        }
-        var tuples = new int[total][];
-        var ti = 0;
-        for (int y = 0; y < nums.size(); y++) {
-            var row = nums.get(y);
-            for (int x = 0; x < row.size(); x++) {
-                tuples[ti++] = new int[]{x + y, y, row.get(x)};
+            int rs = row.size();
+            total += rs;
+            if (rs > maxrs) {
+                maxrs = rs;
             }
         }
-        Arrays.sort(tuples, (t1, t2) -> {
-            if (t1[0] == t2[0]) {
-                return t2[1] - t1[1];
-            } else {
-                return t1[0] - t2[0];
+        var diagonals = new int[nums.size() + maxrs][];
+        for (int rown = nums.size() - 1; rown >= 0; rown--) {
+            var row = nums.get(rown);
+            for (int coln = 0; coln < row.size(); coln++) {
+                var sum = rown + coln;
+                var val = row.get(coln);
+                var diag = diagonals[sum];
+                if (diag != null) {
+                    if (diag[0] == diag.length) {
+                        diag = Arrays.copyOf(diag, (int) (diag.length * 1.4 + 8));
+                        diagonals[sum] = diag;
+                    }
+                    diag[diag[0]++] = val;
+                } else {
+                    diag = new int[8];
+                    diag[0] = 1;
+                    diag[diag[0]++] = val;
+                    diagonals[sum] = diag;
+                }
             }
-        });
-        var accum = new int[ti];
-        for (int i = 0; i < ti; i++) {
-            accum[i] = tuples[i][2];
         }
-        return accum;
+        int[] result = new int[total];
+        int ri = 0;
+        for (int[] diag : diagonals) {
+            if (diag != null) {
+                for (int i = 1; i < diag[0]; i++) {
+                    result[ri++] = diag[i];
+                }
+            }
+        }
+        return result;
     }
+
 }
