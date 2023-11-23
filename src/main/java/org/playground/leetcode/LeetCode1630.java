7e33c5ec284f3705ba7283e1f558b86a3346b5bc
Anton Ustyuzhanin <tony.brewerio@gmail.com>
2023-11-23 21:52:40 +0100

    accepted solution for 1630

--- src/main/java/org/playground/leetcode/LeetCode1630.java
+++ src/main/java/org/playground/leetcode/LeetCode1630.java
@@ -5,24 +5,45 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
- * @see <a href="https://leetcode.com/problems/arithmetic-subarrays/submissions/1105070814/">Submission</a>
- * Runtime: 20 ms, Beats 39.39% of users with Java
- * Memory: 44.58 MB, Beats 25.62% of users with Java
+ * @see <a href="https://leetcode.com/problems/arithmetic-subarrays/submissions/1105078475/">Submission</a>
+ * Runtime: 8 ms, Beats 98.35% of users with Java
+ * Memory: 44.20 MB, Beats 61.71% of users with Java
  * <p>
- * Pretty simple one, got accepted on the first try.
- * Though performance is not good for some reason. Maybe there's a good way to avoid sorting?
+ * Added a couple of tricks.
+ * First, end early if we know for sure that max-min is now divisible by subarray length.
+ * Second, allocate a single buf array in advance and reuse it, instead of new one all the time.
+ * The last one is probably useless, but maybe not.
+ * The end solution is pretty fast, though not that memory efficient.
  */
 public class LeetCode1630 {
 
     public List<Boolean> checkArithmeticSubarrays(int[] nums, int[] l, int[] r) {
+        var buf = Arrays.copyOf(nums, nums.length);
         var result = new ArrayList<Boolean>();
         for (int i = 0; i < l.length; i++) {
-            var arr = Arrays.copyOfRange(nums, l[i], r[i] + 1);
-            Arrays.sort(arr);
+            var min = Integer.MAX_VALUE;
+            var max = Integer.MIN_VALUE;
+            for (int j = l[i]; j < (r[i] + 1); j++) {
+                min = Math.min(min, nums[j]);
+                max = Math.max(max, nums[j]);
+            }
+            if (min == max) {
+                result.add(true);
+                continue;
+            }
+            if ((max - min) % (r[i] - l[i]) != 0) {
+                result.add(false);
+                continue;
+            }
+            //
+            for (int j = l[i]; j < (r[i] + 1); j++) {
+                buf[j] = nums[j];
+            }
+            Arrays.sort(buf, l[i], r[i] + 1);
             boolean ok = true;
-            int match = arr[1] - arr[0];
-            for (int j = 1; j < (arr.length - 1); j++) {
-                if ((arr[j + 1] - arr[j]) != match) {
+            int match = buf[l[i] + 1] - buf[l[i]];
+            for (int j = l[i] + 1; j < r[i]; j++) {
+                if ((buf[j + 1] - buf[j]) != match) {
                     ok = false;
                     break;
                 }
