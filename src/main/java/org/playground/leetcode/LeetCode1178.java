3fbd9fbe06eff1514d479c560715514c69b8fa05
Anton Ustyuzhanin <tony.brewerio@gmail.com>
2023-11-25 23:48:47 +0100

    accepted solution for 1178, uses custom int map, very fast

--- src/main/java/org/playground/leetcode/LeetCode1178.java
+++ src/main/java/org/playground/leetcode/LeetCode1178.java
@@ -3,32 +3,29 @@ package org.playground.leetcode;
 import org.playground.leetcode.helpers.AveragingOperationTimer;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.List;
 
 /**
- * @see <a href="https://leetcode.com/problems/number-of-valid-words-for-each-puzzle/submissions/1106341981/">Submission</a>
- * Runtime: 58 ms, Beats 56.41% of users with Java
- * Memory: 71.75 MB, Beats 15.38% of users with Java
+ * @see <a href="https://leetcode.com/problems/number-of-valid-words-for-each-puzzle/submissions/1106380606/">Submission</a>
+ * Runtime: 34 ms, Beats 100.00% of users with Java
+ * Memory: 69.92 MB, Beats 94.87% of users with Java
  * <p>
  * After googling around, I found how it iterate over bitsets.
  * This can replace & tests over each and every word with a simple hashmap lookup for each subset.
  * Since puzzles are small, they will have very limited amount of subsets, so it is a lot less operations overall.
- * The thing is still slow somehow, what else to do?
+ * <p>
+ * Went a bit too far by creating a specialized counting map using primitive arrays.
+ * The solution is now very fast, but this can be called cheating perhaps.
  */
 public class LeetCode1178 {
     private AveragingOperationTimer timer = new AveragingOperationTimer(LeetCode1178.class);
 
     public List<Integer> findNumOfValidWords(String[] words, String[] puzzles) {
-        var wmap = new HashMap<Integer, Integer>(1 << 8);
-        for (var word : words) {
-            var wsetAndUniqCount = toIntBitsWithUniqCount(word);
-            var wset = wsetAndUniqCount[0];
-            var uniq = wsetAndUniqCount[1];
-            if (uniq > 7) {
-                continue;
-            }
-            wmap.put(wset, wmap.getOrDefault(wset, 0) + 1);
+        var wmap = new IntCountingMap(Math.max(words.length / 8, 8));
+        for (String word : words) {
+            var wset = toIntBits(word);
+            wmap.increment(wset);
         }
         var result = new ArrayList<Integer>(words.length);
         for (var puzzle : puzzles) {
@@ -38,7 +35,7 @@ public class LeetCode1178 {
             int first = 1 << (puzzle.charAt(0) - 'a');
             while (submask > 0) {
                 if ((submask & first) != 0) {
-                    count += wmap.getOrDefault(submask, 0);
+                    count += wmap.get(submask);
                 }
                 submask = (submask - 1) & pset;
             }
@@ -47,17 +44,59 @@ public class LeetCode1178 {
         return result;
     }
 
-    private int[] toIntBitsWithUniqCount(String s) {
-        var set = 0;
-        var uniq = 0;
-        for (int i = 0; i < s.length(); i++) {
-            var b = (1 << (s.charAt(i) - 'a'));
-            if ((set & b) == 0) {
-                set = set | b;
-                uniq++;
+    public static class IntCountingMap {
+        private final int mod;
+        private final int[][] buckets;
+
+        public IntCountingMap(int mod) {
+            this.mod = mod;
+            this.buckets = new int[mod][];
+        }
+
+        public void increment(int n) {
+            var bi = n % mod;
+            int[] bucket = buckets[bi];
+            if (bucket == null) {
+                bucket = new int[8 * 2 + 1];
+                bucket[0] = 3;
+                bucket[1] = n;
+                bucket[2] = 1;
+                buckets[bi] = bucket;
+            } else {
+                for (int i = 1; i < bucket[0]; i += 2) {
+                    if (bucket[i] == n) {
+                        bucket[i + 1]++;
+                        return;
+                    }
+                }
+                if ((bucket[0] + 2) <= bucket.length) {
+                    bucket[bucket[0]] = n;
+                    bucket[bucket[0] + 1] = 1;
+                    bucket[0] += 2;
+                } else {
+                    var size = (int) (bucket.length * 1.5 + 8);
+                    size = (size % 2 == 0) ? size + 1 : size;
+                    bucket = Arrays.copyOf(bucket, size);
+                    bucket[bucket[0]] = n;
+                    bucket[bucket[0] + 1] = 1;
+                    bucket[0] += 2;
+                    buckets[bi] = bucket;
+                }
             }
         }
-        return new int[]{set, uniq};
+
+        public int get(int n) {
+            var bi = n % mod;
+            int[] bucket = buckets[bi];
+            if (bucket != null) {
+                for (int i = 1; i < bucket[0]; i += 2) {
+                    if (bucket[i] == n) {
+                        return bucket[i + 1];
+                    }
+                }
+            }
+            return 0;
+        }
     }
 
     private int toIntBits(String s) {
