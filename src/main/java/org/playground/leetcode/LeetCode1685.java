522bbdcc1b020a1d2d05a34508d775cede5cbb3f
Anton Ustyuzhanin <tony.brewerio@gmail.com>
2023-11-25 13:57:59 +0100

    accepted solution for 1685, very fast, modifies inputs

--- src/main/java/org/playground/leetcode/LeetCode1685.java
+++ src/main/java/org/playground/leetcode/LeetCode1685.java
@@ -2,33 +2,42 @@ package org.playground.leetcode;
 
 /**
  * @see <a href="https://leetcode.com/problems/sum-of-absolute-differences-in-a-sorted-array/submissions/1106042427/">Submission</a>
- * Runtime: 5 ms, Beats 25.64% of users with Java
- * Memory: 57.71 MB, Beats 60.26% of users with Java
+ * Runtime: 4 ms, Beats 65.38% of users with Java
+ * Memory: 56.18 MB, Beats 94.87% of users with Java
  * <p>
  * Feels like it should be faster, comparatively speaking, but perhaps not.
  * Maybe there's a way to use only one sums array?
  * <p>
  * Indeed, it is possible to have just one sums array.
  * Still a bit slow though?
+ * <p>
+ * Now that I think about it, without separate right hand sum array, there is no need for a sums array per se.
+ * Since we iterate from the left side, we can simply accumulate current sum while iterating.
+ * Still need to calculate max sum for right hand calculation, but no need for an array.
+ * <p>
+ * This is the cursed version, modifying the input array.
+ * At 4 ms it is probably withing a margin of error from 3 ms which seems to be the best you can get.
  */
 public class LeetCode1685 {
 
     public int[] getSumAbsoluteDifferences(int[] nums) {
-        int[] sums = new int[nums.length];
-        sums[0] = nums[0];
-        for (int i = 1; i < nums.length; i++) {
-            sums[i] = nums[i] + sums[i - 1];
+        var total = 0;
+        for (int num : nums) {
+            total += num;
         }
-        int[] result = new int[nums.length];
+        int accum = 0;
         for (int i = 0; i < nums.length; i++) {
+            int n = nums[i];
+            nums[i] = 0;
             if (i > 0) {
-                result[i] += nums[i] * i - sums[i - 1];
+                nums[i] += n * i - accum;
             }
             if (i < (nums.length - 1)) {
-                result[i] += (sums[nums.length - 1] - sums[i]) - nums[i] * (nums.length - 1 - i);
+                nums[i] += (total - accum - n) - n * (nums.length - 1 - i);
             }
+            accum += n;
         }
-        return result;
+        return nums;
     }
 
 }
