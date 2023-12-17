afce88aba09c2ab77f3c3d92768128b3b718496b
Anton Ustyuzhanin <tony.brewerio@gmail.com>
2023-12-17 17:26:35 +0100

    accepted solution for 2353, a bit cleaner with dedicated map class

--- src/main/java/org/playground/leetcode/LeetCode2353.java
+++ src/main/java/org/playground/leetcode/LeetCode2353.java
@@ -3,12 +3,15 @@ package org.playground.leetcode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.TreeSet;
 
 /**
- * @see <a href="https://leetcode.com/problems/design-a-food-rating-system/submissions/1121846086//">Submission</a>
- * Runtime: 216 ms, Beats 65.54% of users with Java
- * Memory: 74.04 MB, Beats 32.77% of users with Java ( varies greatly between runs )
+ * @see <a href="https://leetcode.com/problems/design-a-food-rating-system/submissions/1121916196/">Submission</a>
+ * Runtime: 211 ms, Beats 69.49% of users with Java
+ * Memory: 74.90 MB, Beats 23.73% of users with Java ( varies greatly between runs )
  * <p>
  * This one was a little bit more complicated than easy ones before.
  */
@@ -20,45 +23,55 @@ public class LeetCode2353 {
     }
 
     class FoodRatings {
-        private Map<String, TreeMap<Integer, PriorityQueue<String>>> ratingMapByCuisine;
-        private Map<String, TreeMap<Integer, PriorityQueue<String>>> ratingMapByFood;
-        private Map<String, Integer> foodRatingByFood;
+        private Map<String, FoodRatingMap> ratingMapByCuisine;
+        private Map<String, FoodRatingMap> ratingMapByFood;
 
         public FoodRatings(String[] foods, String[] cuisines, int[] ratings) {
             ratingMapByCuisine = new HashMap<>();
-            ratingMapByFood = new HashMap<>();
-            foodRatingByFood = new HashMap<>();
+            ratingMapByFood = new HashMap<>(foods.length);
             for (int i = 0; i < foods.length; i++) {
                 String food = foods[i];
                 String cuisine = cuisines[i];
                 int rating = ratings[i];
-                var map = ratingMapByCuisine.computeIfAbsent(cuisine, c -> new TreeMap<>());
-                map.computeIfAbsent(-1 * rating, r -> new PriorityQueue<>()).add(food);
+                var map = ratingMapByCuisine.computeIfAbsent(cuisine, c -> new FoodRatingMap());
+                map.add(food, rating);
                 ratingMapByFood.put(food, map);
-                foodRatingByFood.put(food, -1 * rating);
             }
         }
 
-        public void changeRating(String food, int newRating) {
+        public void changeRating(String food, int rating) {
             var map = ratingMapByFood.get(food);
-            var prevRating = foodRatingByFood.get(food);
-            var prevQueue = map.get(prevRating);
-            prevQueue.remove(food);
-            if (prevQueue.isEmpty()) {
-                map.remove(prevRating);
-            }
-            var nextQueue = map.computeIfAbsent(-1 * newRating, r -> new PriorityQueue<>());
-            nextQueue.add(food);
-            foodRatingByFood.put(food, -1 * newRating);
+            map.change(food, rating);
         }
 
         public String highestRated(String cuisine) {
-            for (PriorityQueue<String> queue : ratingMapByCuisine.get(cuisine).values()) {
-                for (String food : queue) {
-                    return food;
-                }
+            return ratingMapByCuisine.get(cuisine).highest();
+        }
+
+        public static class FoodRatingMap {
+            private TreeMap<Integer, TreeSet<String>> foodsByRating = new TreeMap<>();
+            private Map<String, Integer> ratingByFood = new HashMap<>();
+
+            public void add(String food, int rating) {
+                var map = foodsByRating.computeIfAbsent(rating, r -> new TreeSet<>());
+                map.add(food);
+                ratingByFood.put(food, rating);
             }
-            return null;
+
+            public void change(String food, int rating) {
+                var prevRating = ratingByFood.put(food, rating);
+                var prevSet = foodsByRating.get(prevRating);
+                prevSet.remove(food);
+                if (prevSet.isEmpty()) {
+                    foodsByRating.remove(prevRating);
+                }
+                var map = foodsByRating.computeIfAbsent(rating, r -> new TreeSet<>());
+                map.add(food);
+            }
+
+            public String highest() {
+                return foodsByRating.lastEntry().getValue().first();
+            }
         }
     }
 }
