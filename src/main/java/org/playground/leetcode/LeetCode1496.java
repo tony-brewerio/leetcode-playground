8aa7e0e33d9049179c9a1bd9d3ec9d7d37a48886
Anton Ustyuzhanin <tony.brewerio@gmail.com>
2023-12-23 12:16:58 +0100

    accepted solution for 1496, optimized by using hand written point class

--- src/main/java/org/playground/leetcode/LeetCode1496.java
+++ src/main/java/org/playground/leetcode/LeetCode1496.java
@@ -6,17 +6,20 @@ import org.slf4j.LoggerFactory;
 import java.util.HashSet;
 
 /**
- * @see <a href="https://leetcode.com/problems/path-crossing/submissions/1126255246/">Submission</a>
- * Runtime: 14 ms, Beats 5.73% of users with Java
- * Memory: 43.34 MB, Beats 5.7% of users with Java ( varies greatly between runs )
+ * @see <a href="https://leetcode.com/problems/path-crossing/submissions/1126537855/">Submission</a>
+ * Runtime: 1 ms, Beats 96.48% of users with Java
+ * Memory: 42.09 MB, Beats 6.17% of users with Java ( varies greatly between runs )
  * <p>
  * Very slow, relatively. There must be an algorithmic trick solution to this.
+ * <p>
+ * Apparently, putting records in a hashmap is bad idea, because they are slow.
+ * Whatever implementation records use for equals and hashcode is way slower than hand rolled one.
  */
 public class LeetCode1496 {
     private final Logger log = LoggerFactory.getLogger(LeetCode1496.class);
 
     public boolean isPathCrossing(String path) {
-        var visited = new HashSet<Point>();
+        var visited = new HashSet<Point>(path.length());
         Point prev = new Point(0, 0);
         visited.add(prev);
         for (int i = 0; i < path.length(); i++) {
@@ -41,6 +44,28 @@ public class LeetCode1496 {
         return false;
     }
 
-    private record Point(int x, int y) {
+    public static class Point {
+        public final int x;
+        public final int y;
+        private final int hc;
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+            this.hc = x + y;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj instanceof Point other) {
+                return other.x == this.x && other.y == this.y;
+            }
+            return false;
+        }
+
+        @Override
+        public int hashCode() {
+            return hc;
+        }
     }
 }
