9807db2dec81fecfbf8fca32cc42920a31492c9b
Anton Ustyuzhanin <tony.brewerio@gmail.com>
2024-01-05 19:57:26 +0100

    accepted solution for 300, optimized a bit, now its good enough

--- src/main/java/org/playground/leetcode/LeetCode300.java
+++ src/main/java/org/playground/leetcode/LeetCode300.java
@@ -6,42 +6,35 @@ import org.slf4j.LoggerFactory;
 import java.util.Arrays;
 
 /**
- * @see <a href="https://leetcode.com/problems/longest-increasing-subsequence/submissions/1137863230/">Submission</a>
- * Runtime: 1194 ms, Beats 5.02% of users with Java
- * Memory: 44.26 MB, Beats 22.77% of users with Java ( varies greatly between runs )
+ * @see <a href="https://leetcode.com/problems/longest-increasing-subsequence/submissions/1137880723/">Submission</a>
+ * Runtime: 54 ms, Beats 27.39% of users with Java
+ * Memory: 43.81 MB, Beats 38.65% of users with Java ( varies greatly between runs )
  * <p>
  * Passes the tests, but is really really slow.
+ * <p>
+ * The second version is the same thing but simplified and reduced to a nested for loop.
+ * Is still slow-ish, but good enough.
  */
 public class LeetCode300 {
     private final Logger log = LoggerFactory.getLogger(LeetCode300.class);
 
     public int lengthOfLIS(int[] nums) {
         int[] cache = new int[nums.length];
-        lengthOfLIS(nums, 0, 1, cache);
-        int result = 0;
-        for (int c : cache) {
-            result = Math.max(result, c);
+        Arrays.fill(cache, 1);
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (cache[i] >= cache[j] && nums[i] < nums[j]) {
+                    cache[j] = cache[i] + 1;
+                }
+            }
+        }
+        int result = 1;
+        for (int count : cache) {
+            if (count > result) {
+                result = count;
+            }
         }
         return result;
     }
 
-    public void lengthOfLIS(int[] nums, int i, int count, int[] cache) {
-        if (cache[i] >= count) {
-            return;
-        }
-        cache[i] = count;
-        int prev = nums[i];
-        for (int j = i + 1; j < nums.length; j++) {
-            int num = nums[j];
-            if (num > prev) {
-                if (cache[j] <= count) {
-                    lengthOfLIS(nums, j, count + 1, cache);
-                }
-            } else{
-                if (cache[j] == 0) {
-                    lengthOfLIS(nums, j, 1, cache);
-                }
-            }
-        }
-    }
 }
