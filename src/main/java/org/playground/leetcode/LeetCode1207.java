ff1da004fd1d99eee70564fa1199cba58bfa2591
Anton Ustyuzhanin <tony.brewerio@gmail.com>
2024-01-17 18:56:53 +0100

    accepted solution for 1207, optimized for speed

--- src/main/java/org/playground/leetcode/LeetCode1207.java
+++ src/main/java/org/playground/leetcode/LeetCode1207.java
@@ -9,20 +9,32 @@ import java.util.HashSet;
 import java.util.Random;
 
 /**
- * @see <a href="https://leetcode.com/problems/unique-number-of-occurrences/submissions/1149022397/">Submission</a>
- * Runtime: 2 ms, Beats 98.10% of users with Java
- * Memory: 42.10 MB, Beats 5.39% of users with Java ( varies greatly between runs )
+ * @see <a href="https://leetcode.com/problems/unique-number-of-occurrences/submissions/1149031911/">Submission</a>
+ * Runtime: 1 ms, Beats 99.44% of users with Java
+ * Memory: 42.52 MB, Beats 5.39% of users with Java ( varies greatly between runs )
  * <p>
  * Pretty clean, and fast enough.
+ * <p>
+ * Updated the solution to use fixed size raw arrays that match input constraints.
+ * Is faster that naive usages of hashmaps.
  */
 public class LeetCode1207 {
     private final Logger log = LoggerFactory.getLogger(LeetCode1207.class);
 
     public boolean uniqueOccurrences(int[] arr) {
-        var counts = new HashMap<Integer, Integer>();
+        var counts = new int[2001];
         for (int num : arr) {
-            counts.merge(num, 1, Integer::sum);
+            counts[num + 1000]++;
         }
-        return new HashSet<>(counts.values()).size() == counts.size();
+        var cache = new boolean[2001];
+        for (int count : counts) {
+            if (count > 0) {
+                if (cache[count]) {
+                    return false;
+                }
+                cache[count] = true;
+            }
+        }
+        return true;
     }
 }
