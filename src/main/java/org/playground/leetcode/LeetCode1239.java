6a578b14b391c5e60dc24c67574f1751e709f6ec
Anton Ustyuzhanin <tony.brewerio@gmail.com>
2024-01-23 21:03:58 +0100

    accepted solution for 1239, much faster

--- src/main/java/org/playground/leetcode/LeetCode1239.java
+++ src/main/java/org/playground/leetcode/LeetCode1239.java
@@ -6,27 +6,40 @@ import org.slf4j.LoggerFactory;
 import java.util.*;
 
 /**
- * @see <a href="https://leetcode.com/problems/set-mismatch/submissions/1154018204/">Submission</a>
- * Runtime: 2 ms, Beats 97.67% of users with Java
- * Memory: 45.14 MB, Beats 70.30% of users with Java ( varies greatly between runs )
+ * @see <a href="https://leetcode.com/problems/maximum-length-of-a-concatenated-string-with-unique-characters/submissions/1154940865/">Submission</a>
+ * Runtime: 2 ms, Beats 98.99% of users with Java
+ * Memory: 40.59 MB, Beats 96.67% of users with Java ( varies greatly between runs )
+ * <p>
+ * Overdone manual bit manipulation version, not HashSet no BitSet.
+ * Runs plenty fast though.
  */
 public class LeetCode1239 {
     private final Logger log = LoggerFactory.getLogger(LeetCode1239.class);
 
     public int maxLength(List<String> arr) {
-        List<BitSet> bitSetList = new ArrayList<>();
+        List<Integer> bitMaskList = new ArrayList<>();
+        outer:
         for (String s : arr) {
-            BitSet bs = new BitSet();
+            int bm = 0;
+            boolean[] seen = new boolean[26];
             for (int i = 0; i < s.length(); i++) {
-                bs.set(s.charAt(i) - 'a');
+                int c = s.charAt(i) - 'a';
+                if (seen[c]) {
+                    continue outer;
+                }
+                seen[c] = true;
+                bm = bm | (1 << c);
             }
-            if (s.length() == bs.cardinality()) {
-                bitSetList.add(bs);
-            }
+            bitMaskList.add(bm);
+            bitMaskList.add(s.length());
+        }
+        int[] bitMaskArray = new int[bitMaskList.size()];
+        for (int i = 0; i < bitMaskList.size(); i++) {
+            bitMaskArray[i] = bitMaskList.get(i);
         }
         int result = 0;
-        for (int i = 0; i < bitSetList.size(); i++) {
-            int candidate = maxLength(bitSetList, i, bitSetList.get(i));
+        for (int i = 0; i < bitMaskArray.length; i += 2) {
+            int candidate = maxLength(bitMaskArray, i, bitMaskArray[i], 0);
             if (candidate > result) {
                 result = candidate;
             }
@@ -34,13 +47,14 @@ public class LeetCode1239 {
         return result;
     }
 
-    private int maxLength(List<BitSet> bitSetList, int from, BitSet current) {
-        current.or(bitSetList.get(from));
-        int result = current.cardinality();
-        for (int i = from + 1; i < bitSetList.size(); i++) {
-            BitSet next = bitSetList.get(i);
-            if (!next.intersects(current)) {
-                int candidate = maxLength(bitSetList, i, (BitSet) current.clone());
+    private int maxLength(int[] bitMaskArray, int from, int prevBitMask, int prevLength) {
+        int currBitMask = prevBitMask | bitMaskArray[from];
+        int currLength = prevLength + bitMaskArray[from + 1];
+        int result = currLength;
+        for (int i = from + 2; i < bitMaskArray.length; i += 2) {
+            int nextBitMask = bitMaskArray[i];
+            if ((nextBitMask & currBitMask) == 0) {
+                int candidate = maxLength(bitMaskArray, i, currBitMask, currLength);
                 if (candidate > result) {
                     result = candidate;
                 }
