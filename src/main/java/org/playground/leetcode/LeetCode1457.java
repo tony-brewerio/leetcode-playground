a6245067e412b90675620c14c55f6a46f1c9b77d
Anton Ustyuzhanin <tony.brewerio@gmail.com>
2024-01-24 21:29:30 +0100

    accepted solution for 1457, optimized a bit

--- src/main/java/org/playground/leetcode/LeetCode1457.java
+++ src/main/java/org/playground/leetcode/LeetCode1457.java
@@ -6,42 +6,37 @@ import org.slf4j.LoggerFactory;
 import java.util.Arrays;
 
 /**
- * @see <a href="https://leetcode.com/problems/pseudo-palindromic-paths-in-a-binary-tree/submissions/1155898953/">Submission</a>
- * Runtime: 12 ms, Beats 72.66% of users with Java
- * Memory: 70.14 MB, Beats 49.06% of users with Java ( varies greatly between runs )
+ * @see <a href="https://leetcode.com/problems/pseudo-palindromic-paths-in-a-binary-tree/submissions/1155902468/">Submission</a>
+ * Runtime: 9 ms, Beats 86.89% of users with Java
+ * Memory: 70.64 MB, Beats 39.33% of users with Java ( varies greatly between runs )
  * <p>
  * Not that fast, but fast enough.
+ * <p>
+ * Optimized a bit by keeping odd item count separately, so there is no need to walk over odds on leaf nodes.
  */
 public class LeetCode1457 {
     private final Logger log = LoggerFactory.getLogger(LeetCode1457.class);
 
     public int pseudoPalindromicPaths(TreeNode root) {
-        return pseudoPalindromicPaths(root, new boolean[10]);
+        return pseudoPalindromicPaths(root, new boolean[10], 0);
     }
 
-    private int pseudoPalindromicPaths(TreeNode node, boolean[] odds) {
-        odds[node.val] = !odds[node.val];
+    private int pseudoPalindromicPaths(TreeNode node, boolean[] odds, int oddItemCount) {
+        if (odds[node.val] = !odds[node.val]) {
+            oddItemCount++;
+        } else {
+            oddItemCount--;
+        }
         if (node.left == null && node.right == null) {
-            boolean found = false;
-            for (boolean odd : odds) {
-                if (odd) {
-                    if (found) {
-                        odds[node.val] = !odds[node.val];
-                        return 0;
-                    } else {
-                        found = true;
-                    }
-                }
-            }
             odds[node.val] = !odds[node.val];
-            return 1;
+            return oddItemCount <= 1 ? 1 : 0;
         }
         int result = 0;
         if (node.left != null) {
-            result += pseudoPalindromicPaths(node.left, odds);
+            result += pseudoPalindromicPaths(node.left, odds, oddItemCount);
         }
         if (node.right != null) {
-            result += pseudoPalindromicPaths(node.right, odds);
+            result += pseudoPalindromicPaths(node.right, odds, oddItemCount);
         }
         odds[node.val] = !odds[node.val];
         return result;
