a4c60ce9057e2fc9723b1aae942cca6f6bedf13f
Anton Ustyuzhanin <tony.brewerio@gmail.com>
2024-02-06 18:42:13 +0100

    accepted solution for 49, now a little faster

--- src/main/java/org/playground/leetcode/LeetCode49.java
+++ src/main/java/org/playground/leetcode/LeetCode49.java
@@ -9,28 +9,44 @@ import java.util.HashMap;
 import java.util.List;
 
 /**
- * @see <a href="https://leetcode.com/problems/group-anagrams/submissions/1168007206/">Submission</a>
- * Runtime: 8 ms, Beats 38.79% of users with Java
- * Memory: 47.75 MB, Beats 35.61% of users with Java ( varies greatly between runs )
- * <p>
- * Works, but is kind of slow-ish.
+ * @see <a href="https://leetcode.com/problems/group-anagrams/submissions/1168032318/">Submission</a>
+ * Runtime: 5 ms, Beats 99.55% of users with Java
+ * Memory: 47.56 MB, Beats 54.86% of users with Java ( varies greatly between runs )
  */
 public class LeetCode49 {
     private final Logger log = LoggerFactory.getLogger(LeetCode49.class);
 
     public List<List<String>> groupAnagrams(String[] strs) {
-        HashMap<String, List<String>> groups = new HashMap<>();
+        HashMap<AnagramKey, List<String>> groups = new HashMap<>();
         for (String str : strs) {
-            char[] key = str.toCharArray();
-            Arrays.sort(key);
-            groups.compute(String.valueOf(key), (k, v) -> {
-                if (v == null) {
-                    v = new ArrayList<>();
-                }
-                v.add(str);
-                return v;
-            });
+            groups.computeIfAbsent(new AnagramKey(str), k -> new ArrayList<>()).add(str);
         }
         return List.copyOf(groups.values());
+    }
+
+    public class AnagramKey {
+        private final byte[] counts;
+        private final int hc;
+
+        public AnagramKey(String str) {
+            counts = new byte[26];
+            for (int i = 0; i < str.length(); i++) {
+                counts[str.charAt(i) - 'a']++;
+            }
+            hc = Arrays.hashCode(counts);
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj instanceof AnagramKey o) {
+                return Arrays.equals(counts, o.counts);
+            }
+            return false;
+        }
+
+        @Override
+        public int hashCode() {
+            return hc;
+        }
     }
 }
